54fd52ac9b6d4377772a884baef869395c9d8913
Anthony Clark <anthonyjclark@gmail.com>
2023-08-11 14:46:11 -0700

    Updated inference (working).

--- inference.py
+++ inference.py
@@ -1,11 +1,25 @@
+import pathlib
 from argparse import ArgumentParser
+from contextlib import contextmanager
 from math import radians
 from pathlib import Path
+from time import sleep
 
+from fastai.callback.wandb import WandbCallback
 from fastai.vision.learner import load_learner
 from ue5osc import Communicator
 
-from utils import y_from_filename  # noqa: F401
+from utils import y_from_filename  # noqa: F401 (needed for fastai load_learner)
+
+
+@contextmanager
+def set_posix_windows():
+    posix_backup = pathlib.PosixPath
+    try:
+        pathlib.PosixPath = pathlib.WindowsPath
+        yield
+    finally:
+        pathlib.PosixPath = posix_backup
 
 
 def parse_args():
@@ -21,7 +35,8 @@ def parse_args():
     arg_parser.add_argument(
         "--rotation_amount",
         type=float,
-        default=radians(10),
+        # default=radians(10),
+        default=10.0,
         help="Rotation per action.",
     )
     arg_parser.add_argument(
@@ -36,22 +51,40 @@ def parse_args():
 def main():
     args = parse_args()
 
-    model = load_learner(args.model)
+    # NOTE: This is a workaround for a bug in fastai/pathlib on Windows
+    with set_posix_windows():
+        model = load_learner(args.model)
+
+    # TODO: temporary fix? (we might remove callback on training side)
+    model.remove_cb(WandbCallback)
 
     output_dir = Path(args.output_dir).resolve()
     output_dir.mkdir(parents=True, exist_ok=True)
 
+    # Check if output directory is empty
+    if any(output_dir.iterdir()):
+        print("Output directory is not empty. Aborting.")
+        return
+
     with Communicator("127.0.0.1", ue_port=7447, py_port=7001) as ue:
-        for action_index in range(args.max_actions):
+        print("Connected to", ue.get_project_name())
+        print("Saving images to", output_dir)
+        ue.reset()
+
+        for action_step in range(args.max_actions):
             # Save image
-            image_filename = f"{args.output_dir}/{action_index}.png"
+            image_filename = f"{output_dir}/{action_step:04}.png"
             ue.save_image(image_filename)
+            sleep(0.5)
 
             # Predict correct action
-            predicted_action = model.predict(image_filename)[0]
+            action_to_take, action_index, action_probs = model.predict(image_filename)
+            action_prob = action_probs[action_index]
+
+            print(f"Moving {action_to_take} with probabilities {action_prob:.2f}")
 
             # Take action
-            match predicted_action:
+            match action_to_take:
                 case "forward":
                     ue.move_forward(args.movement_amount)
                 case "left":
@@ -59,7 +92,7 @@ def main():
                 case "right":
                     ue.rotate_right(args.rotation_amount)
                 case _:
-                    raise ValueError(f"Unknown action: {predicted_action}")
+                    raise ValueError(f"Unknown action: {action_to_take}")
 
 
 if __name__ == "__main__":
